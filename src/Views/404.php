1c1a39e3ac37e1013bc3592348026d9ff629f4bf
samuelguebo <samuelguebo@gmail.com>
2020-09-21 03:35:21 +0000

    [UI] 404 Error view

---
+++ src/Views/404.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * File displaying UI
+ * when an error occured
+ */
+?>
+<?php require_once VIEW_DIR . '/header.php';?>
+
+		<h1 class="mt-5"><?php echo APP_NAME; ?></h1>
+		<p class="lead"><?php echo APP_SLOGAN; ?></p>
+		<hr>
+		<p class="tou-description alert alert-warning">An error occured. Please try again.</p>
+
+	<?php require_once VIEW_DIR . '/footer.php';
