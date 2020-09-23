963cb7a6ae3f66cf1eda56a871dcf81cdc481cd8
samuelguebo <samuelguebo@gmail.com>
2020-09-23 11:37:57 +0000

    [UI] Favicon in header

--- src/Views/header.php
+++ src/Views/header.php
@@ -12,7 +12,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title> <?php echo APP_NAME; ?> | <?php echo APP_DESCRIPTION; ?></title>
-
+  <link rel="icon" href="/public/img/favicon.png" sizes="24x24" type="image/png"> 
   <!-- Bootstrap -->
   <link rel="stylesheet" href="//tools-static.wmflabs.org/cdnjs/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="//tools-static.wmflabs.org/cdnjs/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
